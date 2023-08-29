3e20a2a48edc8d5908df43cf2e35f3897470b1c4
Longngok <bboylongngok193@gmail.com>
2023-08-29 16:04:15 +0700

    first commit

---
+++ assets/purgecss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    content: ["index.html", "src/**/*.js"], // Các tệp HTML và JavaScript chứa mã sử dụng CSS
+    css: ["path/to/compiled/styles.css"],   // Tệp CSS sau khi biên dịch từ SASS
+  };
